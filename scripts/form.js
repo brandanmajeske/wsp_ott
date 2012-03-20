0c1376266f5c41dd25f586beb40c3f5c1c58c209
Brandan Majeske <bmajeske@me.com>
2012-03-19 20:31:35 -0600

    Many changes

--- scripts/form.js
+++ scripts/form.js
@@ -6,34 +6,40 @@
 	$(function() {
 		$( "#datepicker" ).datepicker();
 	});
+	
+	$.widget( "ui.timespinner", $.ui.spinner, {
+		options: {
+			// seconds
+			step: 60 * 1000,
+			// hours
+			page: 60
+		},
 
-/*$(document).ready(function() {
-	if (!Modernizr.input.autofocus) {
-		$("input[autofocus]").focus(); 
+		_parse: function( value ) {
+			if ( typeof value === "string" ) {
+				// already a timestamp
+				if ( Number( value ) == value ) {
+					return Number( value );
+				}
+				return +Globalize.parseDate( value );
+			}
+			return value;
+		},
+
+		_format: function( value ) {
+			return Globalize.format( new Date(value), "t" );
 		}
 	});
 
-$(document).ready(function() {
-	if (!Modernizr.inputplaceholder) {
-		makePlaceholders();
-	}
-});
+	$(function() {
+		$( "#spinner" ).timespinner();
 
-$("document").ready(function() {
-	$("input[required]").css("border", "1px solid #0084FF")
-});
-
-$("document").ready(function() {
-	if (!Modernizr.inputtypes.number) {
-		var $numerics = $("input[type=number]");
-		$numerics.each(function () {
-			var thisNum = $(this);
-			thisNum.spinner({
-				min: thisNum.attr("min"),
-				max: thisNum.attr("max"),
-				step: thisNum.attr("step")
-			});
-		}); */
+		$( "#culture" ).change(function() {
+			var current = $( "#spinner" ).timespinner( "value" );
+			Globalize.culture( $(this).val() );
+			$( "#spinner" ).timespinner( "value", current );
+		});
+	});
 
     $("document").ready(function () {
         if (!Modernizr.input.placeholder) {
@@ -63,6 +69,7 @@ $("document").ready(function() {
                 });
             });
         }
-    });
-    
 
+     });
+    
+	
